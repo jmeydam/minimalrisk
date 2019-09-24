3f24874087e20861cb6da65fc216948463d29fe4
Jens Meydam <jmeydam@gmail.com>
2019-09-24 23:06:48 +0200

    More verbose output in MinimalRiskTestdriveStateless.

--- src/MinimalRiskTestdriveStateless.java
+++ src/MinimalRiskTestdriveStateless.java
@@ -14,43 +14,59 @@ public class MinimalRiskTestdriveStateless {
         try {
             String initConfigFile = "json/country_graph_example.json";
 
-            System.out.println("Reading " + initConfigFile);
+            System.out.println("\nReading " + initConfigFile + "\n");
             Path path = FileSystems.getDefault().getPath(initConfigFile);
             String countryGraph = Files.readString(path, StandardCharsets.UTF_8);
 
-            System.out.println("Initial configuration of board");
+            System.out.println("\nInitial configuration of board:\n");
             System.out.println(countryGraph);
 
             countryGraph = MinimalRisk.initialAllocationOfCountries(countryGraph, "A", "B");
-            System.out.println("Initial allocation of countries");
+            System.out.println("\nAfter initialAllocationOfCountries:\n");
             System.out.println(countryGraph);
             
             countryGraph = MinimalRisk.initialAllocationOfExtraTroops(countryGraph, 6);
-            System.out.println("Initial allocation of extra troops");
+            System.out.println("\nAfter initialAllocationOfExtraTroops:\n");
             System.out.println(countryGraph);
-
 
             for (int i = 0; i < 1000; i++) {
                 if (i % 2 == 0) {
+                    System.out.println("\n\n*************************************************\n");
                     System.out.println("\nRound " + (i / 2 + 1) + " ...\n");
-                    System.out.println("A\'s turn\n");
+                    System.out.println("\nA\'s turn\n");
                     // nextMove("A");
                     countryGraph = MinimalRisk.allocationOfExtraTroops(countryGraph, "A", 2);
+                    System.out.println("\nAfter allocationOfExtraTroops:\n");
+                    System.out.println(countryGraph);
                     countryGraph = MinimalRisk.attack(countryGraph, "A");
+                    System.out.println("\nAfter attack:\n");
+                    System.out.println(countryGraph);
                     countryGraph = MinimalRisk.moveTroops(countryGraph, "A");
+                    System.out.println("\nAfter moveTroops:\n");
+                    System.out.println(countryGraph);
                 } else {
                     System.out.println("\nB\'s turn\n");
                     // nextMove("B");
                     countryGraph = MinimalRisk.allocationOfExtraTroops(countryGraph, "B", 2);
+                    System.out.println("\nAfter allocationOfExtraTroops:\n");
+                    System.out.println(countryGraph);
                     countryGraph = MinimalRisk.attack(countryGraph, "B");
+                    System.out.println("\nAfter attack:\n");
+                    System.out.println(countryGraph);
                     countryGraph = MinimalRisk.moveTroops(countryGraph, "B");
+                    System.out.println("\nAfter moveTroops:\n");
+                    System.out.println(countryGraph);
                 }
+                System.out.println("\nGame over?\n");
                 System.out.println(countryGraph);
                 if (MinimalRisk.gameOver(countryGraph)) {
                     System.out.println("\nGAME OVER!\n");
                     break;
+                } else {
+                    System.out.println("\nNo.\n");
                 }
             }
+
         } catch(Exception e) {
             e.printStackTrace();
             System.out.println();
